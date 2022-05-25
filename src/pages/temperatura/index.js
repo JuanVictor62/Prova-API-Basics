f1b7083bf20ad6eb31bbc5c56673a5f166fe7cc9
eoqthiago <contatopixthiago@gmail.com>
2022-05-25 11:55:15 -0300

    style pika

--- src/pages/temperatura/index.js
+++ src/pages/temperatura/index.js
@@ -1,12 +1,39 @@
 import { useState } from 'react'
 
 import axios from 'axios'
+import './index.scss';
 
 export default function Index() {
+    const [temperatura, setTemperatura] = useState(0);
+    const [x, setResposta] = useState('');
+
+    async function febre () {
+        const resp = await axios.get(`http://localhost:5000/temperatura?grau=${temperatura}`)
+
+        if( resp.data.x === true) {
+            setResposta('Sim')
+        }  else {
+            setResposta('Não!')
+        }
+
+    }
+
     return(
-        <div>
-            <h1> Temperatura </h1>
-        </div>
+        <main className='conteiner'>
+            <div className='principal'>
+                <h1> Temperatura </h1>
+                <div className="conteudo">
+                    <input className='input-style' type='text' placeholder='digite a temperatura' onChange={e => setTemperatura(e.target.value)} />
+                </div>
+                <div className='btn'>
+                    <button className='btn-style' onClick={febre}>calcular</button>
+                </div>
+                <div className='resultado'>
+                    febre: {x}
+                </div>
+            </div>
+
+        </main>
     )
         
 }
